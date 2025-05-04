788cc67d6f683fac3529bedf5bcecdb915161152
oussama7007 <oussamatachakourt@gmail.com>
2025-05-04 18:25:16 +0100

    done

---
+++ includes/header.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/04 18:06:01 by oait-si-          #+#    #+#             */
+/*   Updated: 2025/05/04 18:23:20 by oait-si-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+
