581023e960a364b82a3652305af8ad9bacddf46c
root <adouza.13@gmail.com>
2025-05-06 09:35:28 +0100

    new main

--- c_spuvr/Mytest.c
+++ c_spuvr/Mytest.c
@@ -6,7 +6,7 @@
 /*   By: oadouz <oadouz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 15:31:37 by oadouz            #+#    #+#             */
-/*   Updated: 2025/05/05 18:26:14 by oadouz           ###   ########.fr       */
+/*   Updated: 2025/05/06 09:35:15 by oadouz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,18 +54,6 @@ int main(int ac, char **av, char **env)
 	{
 	    write(2, "Error: Failed to initialize environment\n", 40);
 	    return (1);
-	}
-
-	while(copy_envp[j])
-	{
-		i = 0;
-		while(copy_envp[j][i])
-		{
-			write(1, &copy_envp[j][i], 1);
-			i++;
-		}
-		write(1, "\n", 1);
-		j++;
 	}
 	return (0);
 }
