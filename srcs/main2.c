aa7359b27c46f7bb2dc4b90ead731bee8b7e69f6
oussama7007 <oussamatachakourt@gmail.com>
2025-05-06 17:16:59 +0100

    done

--- srcs/main2.c
+++ srcs/main2.c
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 18:26:38 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/05/06 14:57:08 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/05/06 17:16:48 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,36 @@ int ft_strlen(char *line)
     return i;
 }
 
-int     parse_line(char *line)
+int is_space(int  c)
 {
-    while
+    return(c >= 9 &&  c <= 32);
+}
+int     parse_line(t_token *token, char *line)
+{
+    int i;
+    int end;
+    int start;
+    i = 0;
+    while(line[i])
+    {
+        while(is_space(line[i]))
+           i++;
+        start = i;
+        while(line[i] && !is_space(line[i]))
+            i++;
+        end = i ;
+         
+    }
 }
 int main()
 {
-    t_command *cmd;
+    t_token *token;
     
     char *line;
     while(1)
     {
         line = readline("Minishell$ ");
-        if(!parse_line(line))
+        if(!parse_line(token, line))
             return(0);
         free(line);
         
