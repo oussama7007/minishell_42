e62a556db7d44f8693437ce6842339c79dbfe8d2
root <adouza.13@gmail.com>
2025-05-07 09:43:34 +0100

    ppspsps

--- c_spuvr/minishell_structs.h
+++ c_spuvr/minishell_structs.h
@@ -6,7 +6,7 @@
 /*   By: oadouz <oadouz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:18:20 by oadouz            #+#    #+#             */
-/*   Updated: 2025/05/06 18:42:23 by oadouz           ###   ########.fr       */
+/*   Updated: 2025/05/07 09:43:24 by oadouz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,5 @@
 # include "built_functions.h"
 # include "LIBFT/libft.h"
 
-
-char	*ft_strdup(char *s);
-int	    ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_strlen(char *s);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-int		ft_arrlen(char **arr);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
