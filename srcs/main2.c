76211b77d894b7621afd268746c227d6c2418d46
oussama7007 <oussamatachakourt@gmail.com>
2025-05-07 23:16:27 +0100

    done

--- srcs/main2.c
+++ srcs/main2.c
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 18:26:38 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/05/07 21:22:50 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/05/07 23:15:31 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,21 @@ void    t()
 {
     system("leaks a.out");
 }
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while ((i < n) && (s1[i] || s2[i]))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
 int     check_exit(char *line)
 {
     int i;
@@ -77,8 +92,7 @@ int     check_exit(char *line)
         i++;
     if(line[i] == '\0')
         return 1;
-    if(line[i] == 'e' && line[i + 1] == 'x' && 
-             line[i + 2] == 'i' &&  line[i + 3] == 't')
+    if(ft_strncmp(line, "exit", 4) == 0)
     {
         i += 4;
         while(line[i] && is_space(line[i]))
