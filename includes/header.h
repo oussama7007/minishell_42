3643d4954424ffdbfe359d5dd771d6190730dafe
root <adouza.13@gmail.com>
2025-05-07 09:49:58 +0100

    yo

--- includes/header.h
+++ includes/header.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   header.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oadouz <oadouz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 18:06:01 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/05/06 16:03:21 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/05/07 09:49:48 by oadouz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <readline/readline.h>
-#include <readline/readline.h>
 #include <readline/history.h>
 #include <unistd.h>
 #include <stdbool.h>
-
-// ana ankhedm bhadi 
-
 
 
 #define TOKEN_WORD 0
