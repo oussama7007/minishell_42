22b3de0eb1f3d365ddd701acf8fd8c53cdcc787e
oussama7007 <oussamatachakourt@gmail.com>
2025-05-07 16:03:03 +0100

    done

--- srcs/main2.c
+++ srcs/main2.c
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 18:26:38 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/05/06 17:16:48 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/05/06 21:35:48 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,28 @@ int ft_strlen(char *line)
 
 int is_space(int  c)
 {
-    return(c >= 9 &&  c <= 32);
+    return(c >= 9 && c <= 32);
+}
+int check_line(char *line)
+{
+    int i;
+    
+    i = 0;
+    while(line[i])
+    {
+        if(line[i] == '|')
+            return TOKEN_PIPE;
+        else if(line[i] == '>')
+    }
 }
 int     parse_line(t_token *token, char *line)
 {
     int i;
     int end;
     int start;
+    char *line;
+    char token_value;
+    
     i = 0;
     while(line[i])
     {
@@ -39,17 +54,30 @@ int     parse_line(t_token *token, char *line)
         while(line[i] && !is_space(line[i]))
             i++;
         end = i ;
-         
+        if(start < end)
+        {
+            line = ft_strdup(start, end, line);
+            if(!line)
+                return 0;
+        }
+        token_value = check_line(line);
+        add_token(&token, token_value);
     }
 }
 int main()
 {
+    char *line;
     t_token *token;
     
-    char *line;
+   
     while(1)
     {
         line = readline("Minishell$ ");
+        if(!line)
+        {
+            write(1,"exit\n",5);
+            exit(0);
+        }
         if(!parse_line(token, line))
             return(0);
         free(line);
