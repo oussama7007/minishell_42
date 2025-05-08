2ab57da34b95c71e68fcdd36f95bc49b89bf7c86
Oussama Adouz <adouza.13@gmail.com>
2025-05-08 17:46:19 +0100

    alr

--- c_spuvr/built_functions.h
+++ c_spuvr/built_functions.h
@@ -6,7 +6,7 @@
 /*   By: oadouz <oadouz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 18:18:27 by oadouz            #+#    #+#             */
-/*   Updated: 2025/05/08 15:27:11 by oadouz           ###   ########.fr       */
+/*   Updated: 2025/05/08 17:46:07 by oadouz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,8 @@ char	*create_env_data(char *name, char *value);
 int		find_var_index(char *str, char**envp);
 int		my_setenv(char *name, char *value, char ***env_ptr);
 int		my_unsetenv(const char *name, char ***env_ptr);
+int		ft_echo(char **args);
+int		ft_cd(char **args, char ***env_ptr);
+int		ft_pwd(void);
 
 #endif
