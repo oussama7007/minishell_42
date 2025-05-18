d44a65534a02f5d9fa97c1bac01e4ddb13738379
Oussama Adouz <adouza.13@gmail.com>
2025-05-18 16:13:24 +0100

    update

--- srcs/c_spuvr/built_functions.h
+++ srcs/c_spuvr/built_functions.h
@@ -6,7 +6,7 @@
 /*   By: oadouz <oadouz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 18:18:27 by oadouz            #+#    #+#             */
-/*   Updated: 2025/05/17 20:02:53 by oadouz           ###   ########.fr       */
+/*   Updated: 2025/05/18 15:06:42 by oadouz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ int		my_setenv(char *name, char *value, char ***env_ptr);
 int		my_unsetenv(const char *name, char ***env_ptr);
 int		ft_export(char **args, char ***env_ptr);
 int		ft_pwd(void);
+int		ft_unset(char **args, char ***env_ptr);
 void	display_sorted_environment(char **envp);
 int		process_export_arguments(char **args, char ***env_ptr);
 int		is_standalone_name(const char *arg);
