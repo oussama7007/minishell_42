fa2a5ff043761d39b44fb8a6e0dcac5106d47921
oussama7007 <oussamatachakourt@gmail.com>
2025-05-20 15:15:24 +0100

    done

--- srcs/main.c
+++ srcs/main.c
@@ -3,16 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oadouz <oadouz@student.42.fr>              +#+  +:+       +#+        */
+/*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 18:26:38 by oait-si-          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/05/19 16:37:35 by oait-si-         ###   ########.fr       */
-=======
-/*   Updated: 2025/05/18 16:50:16 by oadouz           ###   ########.fr       */
->>>>>>> 4a4c5d1c2e30ff4296497563a7c83f26aeee629b
+/*   Updated: 2025/05/20 15:15:03 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 
 #include "includes/header.h"
@@ -145,7 +142,7 @@ int main(int ac, char **av, char **env)
     t_command *commands;
 
 
-    printf("%s\n", env[0]);
+    
 
     (void)ac;
     (void)av;
@@ -185,8 +182,7 @@ int main(int ac, char **av, char **env)
         {
             is_built_ins(commands->args, &my_envp);
         }
-        //print_tokens(tokens); // for dubg
-        // print_commands(commands);
+        print_commands(commands);
         free_tokens(tokens);
         free(line);
     }
