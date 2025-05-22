67b4e682b0b41aa8034d832eae6ba3636665c0ea
Oussama Adouz <adouza.13@gmail.com>
2025-05-22 18:27:12 +0100

    update execute status

--- srcs/c_spuvr/built-ins1.c
+++ srcs/c_spuvr/built-ins1.c
@@ -6,7 +6,7 @@
 /*   By: oadouz <oadouz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 17:54:48 by oadouz            #+#    #+#             */
-/*   Updated: 2025/05/22 15:42:24 by oadouz           ###   ########.fr       */
+/*   Updated: 2025/05/22 18:25:30 by oadouz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,19 @@
 
 int	is_built_ins(char **cmd, char ***env_ptr)
 {
+	int	ret_status;
+
 	if ((ft_strcmp(cmd[0], "export")) == 0)
-		return (ft_export(cmd, env_ptr), 0);
+		return (ft_export(cmd, env_ptr));
 	else if ((ft_strcmp(cmd[0], "pwd")) == 0)
-		return (ft_pwd(env_ptr), 0);
+		return (ft_pwd(env_ptr));
 	else if ((ft_strcmp(cmd[0], "cd")) == 0)
-		return (ft_chdir(cmd, env_ptr), 0);
+		return (ft_chdir(cmd, env_ptr));
 	else if ((ft_strcmp(cmd[0], "unset")) == 0)
-		return (ft_unset(cmd, env_ptr), 0);
+		return (ft_unset(cmd, env_ptr));
 	else if ((ft_strcmp(cmd[0], "env")) == 0)
-		return (ft_env(cmd, env_ptr), 0);
+		return (ft_env(cmd, env_ptr));
 	else if ((ft_strcmp(cmd[0], "echo")) == 0)
-		return (ft_echo(cmd), 0);
-	return (1);
+		return (ft_echo(cmd));
+	return (999);
 }
--- srcs/c_spuvr/execute_heart.c
+++ srcs/c_spuvr/execute_heart.c
@@ -6,7 +6,7 @@
 /*   By: oadouz <oadouz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 15:31:37 by oadouz            #+#    #+#             */
-/*   Updated: 2025/05/22 17:29:08 by oadouz           ###   ########.fr       */
+/*   Updated: 2025/05/22 18:26:53 by oadouz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,11 +39,14 @@ static int	ft_execute_external(t_command *cmd, char **envp)
 int	ft_execute_command_list(t_command *cmd_list, char ***env_ptr)
 {
 	int	status;
+	int	builtin_status;
 
 	if (!cmd_list || !cmd_list->args || !cmd_list->args[0])
 		return (0);
-	if (is_built_ins(cmd_list->args, env_ptr) == 0)
-		return (0);
-	status = ft_execute_external(cmd_list, *env_ptr);
+	builtin_status = is_built_ins(cmd_list->args, env_ptr);
+	if (builtin_status == 999)
+		status = ft_execute_external(cmd_list, *env_ptr);
+	else
+		status = builtin_status;
 	return (status);
 }
