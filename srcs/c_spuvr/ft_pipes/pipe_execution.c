770bbae66d930f55086c03c43c666e000d0e3943
root <adouza.13@gmail.com>
2025-06-09 12:42:25 +0100

    right pipe line

--- srcs/c_spuvr/ft_pipes/pipe_execution.c
+++ srcs/c_spuvr/ft_pipes/pipe_execution.c
@@ -6,7 +6,7 @@
 /*   By: oadouz <oadouz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 17:44:44 by oadouz            #+#    #+#             */
-/*   Updated: 2025/06/04 17:43:54 by oadouz           ###   ########.fr       */
+/*   Updated: 2025/06/09 12:39:24 by oadouz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,45 @@ static void	setup_child_io(int prev_pipe, int *pipe_fds, t_command *cmd)
 	if (prev_pipe != STDIN_FILENO)
 	{
 		if (dup2(prev_pipe, STDIN_FILENO) == -1)
+		{
+			perror("minishell: dup2 stdin");
 			exit(1);
+		}
 		close(prev_pipe);
 	}
 	if (cmd->next)
 	{
 		if (dup2(pipe_fds[1], STDOUT_FILENO) == -1)
+		{
+			perror("minishell: dup2 stdout");
 			exit(1);
+		}
 		close(pipe_fds[1]);
 		close(pipe_fds[0]);
 	}
 }
 
+static void	execute_single_command(t_command *cmd, char ***env_ptr)
+{
+	int	builtin_status;
+
+	if (!cmd || !cmd->args || !cmd->args[0])
+		exit(0);
+	builtin_status = is_built_ins(cmd->args, env_ptr);
+	if (builtin_status == 999)
+	{
+		execute_child_process(find_executable_path(cmd->args[0], *env_ptr), 
+			cmd->args, *env_ptr);
+	}
+	else
+		exit(builtin_status);
+}
+
 static void	execute_child(t_command *cmd, char ***env_ptr, int prev, int *pipe_fd)
 {
-	int	status;
-
 	setup_child_io(prev, pipe_fd, cmd);
 	handle_redirection_child(cmd);
-	status = ft_execute_command_list(cmd, env_ptr);
-	exit(status);
+	execute_single_command(cmd, env_ptr);
 }
 
 static void	handle_parent_pipes(int *prev_pipe, int *pipe_fds, t_command *cmd)
@@ -86,14 +105,19 @@ int	execute_pipeline(t_command *commands, char ***env_ptr)
 	while (cmd)
 	{
 		if (cmd->next && pipe(pipe_fds) == -1)
+		{
+			perror("minishell: pipe");
 			return (1);
+		}
 		pid = fork();
 		if (pid == -1)
+		{
+			perror("minishell: fork");
 			return (1);
+		}
 		if (pid == 0)
 			execute_child(cmd, env_ptr, prev_pipe, pipe_fds);
-		if (!cmd->next)
-			last_pid = pid;
+		last_pid = pid;
 		handle_parent_pipes(&prev_pipe, pipe_fds, cmd);
 		cmd = cmd->next;
 	}
