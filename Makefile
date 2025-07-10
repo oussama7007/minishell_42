186c821a69663c7858f35e0ed150f9bfb7026a43
root <adouza.13@gmail.com>
2025-07-10 22:19:54 +0100

    fixed little error in exit

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 NAME        := minishell
 CC          := cc
-CFLAGS      :=  -fsanitize=address -g #-Wall -Wextra -Werror
+CFLAGS      := -g -fsanitize=address #-Wall -Wextra -Werror
 LINKER      := -lreadline -lncurses
 INCLUDES    := -I srcs/includes
  
--- srcs/c_spuvr/unset_exit/ft_exit.c
+++ srcs/c_spuvr/unset_exit/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: oadouz <oadouz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 17:22:45 by oadouz            #+#    #+#             */
-/*   Updated: 2025/07/09 23:41:03 by oadouz           ###   ########.fr       */
+/*   Updated: 2025/07/10 22:16:54 by oadouz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,21 +48,19 @@ int	ft_exit(char **args, t_exit_data *exit_data)
 {
 	ft_putendl_fd("exit", 1);
 	if (!args[1])
-	{
-		exit_data->data->ex_status = 0;
 		cleanup_and_exit(exit_data);
-	}
 	if (!is_numeric_arg(args[1]))
 	{
 		ft_putstr_fd("minishell: exit: ", 2);
 		ft_putstr_fd(args[1], 2);
 		ft_putendl_fd(": numeric argument required", 2);
-		exit_data->data->ex_status = 255;
+		exit_data->data->ex_status = 2;
 		cleanup_and_exit(exit_data);
 	}
-	if (args[2])
+	if (args[1] && args[2])
 	{
 		ft_putendl_fd("minishell: exit: too many arguments", 2);
+		exit_data->data->ex_status = 1;
 		return (1);
 	}
 	exit_data->data->ex_status = (unsigned char)ft_atoi(args[1]);
